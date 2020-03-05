9bae9395c6d706e8e1f1b9d3a45c53381c3639a1
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-05 21:39:30 +0530

    Update Visual_Data_Analysis.py

--- pandas_Foundations/Visual_Data_Analysis.py
+++ pandas_Foundations/Visual_Data_Analysis.py
@@ -18,3 +18,37 @@ plt.ylabel('Price ($US)')
 
 # Display the plot
 plt.show()
+
+#===================================================================================
+
+#Plotting Scatter Graph 
+# Generate a scatter plot
+# Kind =Scatter , means scatter plots
+df.plot(kind='scatter', x='hp', y='mpg', s=sizes)
+
+# Add the title
+plt.title('Fuel efficiency vs Horse-power')
+
+# Add the x-axis label
+plt.xlabel('Horse-power')
+
+# Add the y-axis label
+plt.ylabel('Fuel efficiency (mpg)')
+
+# Display the plot
+plt.show()
+
+
+#====================================================================================
+# While pandas can plot multiple columns of data in a single figure, 
+# making plots that share the same x and y axes, there are cases where
+# two columns cannot be plotted together because their units do not match.
+# The .plot() method can generate subplots for each column being plotted. Here, each plot will be scaled independently
+# Make a list of the column names to be plotted: cols
+cols = list(['weight','mpg'])
+
+# Generate the box plots
+df[cols].plot(subplots=True)
+
+# Display the plot
+plt.show()
