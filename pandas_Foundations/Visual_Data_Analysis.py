626c20dcbde613a837c0a48ce7f80485f49032ea
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-05 21:04:23 +0530

    Update Visual_Data_Analysis.py

--- pandas_Foundations/Visual_Data_Analysis.py
+++ pandas_Foundations/Visual_Data_Analysis.py
@@ -1,13 +1,16 @@
 #Import Libraries
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Create a list of y-axis column names: y_columns
 y_columns = list(['AAPL','IBM'])
 
 # Generate a line plot
+# Will plot 2 different lines with respect to Month
 df.plot(x='Month', y=y_columns)
 
 # Add the title
+#Using plot() without x and y will set the name/title for the graph
 plt.plot('Monthly stock prices')
 
 # Add the y-axis label
