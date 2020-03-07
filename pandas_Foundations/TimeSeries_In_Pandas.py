336929131037f13d1efcc3745b21dfc6ff46c35a
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-07 22:56:02 +0530

    Update TimeSeries_In_Pandas.py

--- pandas_Foundations/TimeSeries_In_Pandas.py
+++ pandas_Foundations/TimeSeries_In_Pandas.py
@@ -72,5 +72,80 @@ august_highs = august.resample('24h').max()
 # Extract temperature data for February: february
 february = df['Temperature'].loc['2010-02']
 
+
+#======================================================================================
+# Extract the August 2010 data: august
+august = df['Temperature']['2010-08']
+
+# Resample to daily data, aggregating by max: daily_highs
+daily_highs = august.resample('D').max()
+
+# Use a rolling 7-day window with method chaining to smooth the daily high temperatures in August
+daily_highs_smoothed = daily_highs.rolling(window=7).mean()
+print(daily_highs_smoothed)
+
+
+#======================================================================================
+#Manipulating Time Series Data
 # Downsample to obtain the daily lowest temperatures in February: february_lows
 february_lows = february.resample('24h').min()
+
+#===================================================================================
+#Rolling mean and frequency
+# Extract data from 2010-Aug-01 to 2010-Aug-15: unsmoothed
+unsmoothed = df['Temperature']['2010-Aug-01':'2010-Aug-15']
+
+# Apply a rolling mean with a 24 hour window: smoothed
+smoothed = unsmoothed.rolling(window=24).mean()
+
+# Create a new DataFrame with columns smoothed and unsmoothed: august
+august = pd.DataFrame({'smoothed':smoothed, 'unsmoothed':unsmoothed})
+
+# Plot both smoothed and unsmoothed data using august.plot().
+august.plot()
+plt.show()
+
+
+
+#============================================================================
+#Method Chaining and filtering
+# Strip extra whitespace from the column names: df.columns
+df.columns = df.columns.str.strip()
+
+# Extract data for which the destination airport is Dallas: dallas
+dallas = df['Destination Airport'].str.contains('DAL')
+
+# Compute the total number of Dallas departures each day: daily_departures
+daily_departures = dallas.resample('D').sum()
+
+# Generate the summary statistics for daily Dallas departures: stats
+stats = daily_departures.describe()
+
+
+#===================================================================================
+#Missing Values and interpolation
+# Reset the index of ts2 to ts1, and then use linear interpolation to fill in the NaNs: ts2_interp
+ts2_interp = ts2.reindex(ts1.index).interpolate(how="linear")
+
+# Compute the absolute difference of ts1 and ts2_interp: differences 
+differences = np.abs(ts2_interp-ts1)
+
+# Generate and print summary statistics of the differences
+print(differences.describe())
+
+#===================================================================================
+# Time Zones and Conversion
+# Build a Boolean mask to filter for the 'LAX' departure flights: mask
+mask = df['Destination Airport'] == 'LAX'
+
+# Use the mask to subset the data: la
+la = df[mask]
+
+# Combine two columns of data to create a datetime series: times_tz_none 
+times_tz_none = pd.to_datetime( la['Date (MM/DD/YYYY)'] + ' ' + la['Wheels-off Time'] )
+
+# Localize the time to US/Central: times_tz_central
+times_tz_central = times_tz_none.dt.tz_localize('US/Central')
+
+# Convert the datetimes from US/Central to US/Pacific
+times_tz_pacific = times_tz_central.dt.tz_convert('US/Pacific')
