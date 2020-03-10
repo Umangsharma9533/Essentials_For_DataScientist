e2f60aca1f838e19cbf57e9fd483574fe2346f95
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-10 11:02:40 +0530

    Update Matplotlib.py

--- Matplotlib.py
+++ Matplotlib.py
@@ -85,3 +85,24 @@ plt.show()
 #=================================================================
 #add different  color to different country to make it more diffrentiable
 #alpha value ranges from 0 to 1 , 0 means transparent and 1 is opaque
+#plt.text(x_cor,y_cor,"text"): Will add text at a particular mentioned co-ordinates
+#plt.grid(True) : This will add grids to the graphs 
+# Scatter plot
+plt.scatter(x = gdp_cap, y = life_exp, s = np.array(pop) * 2, c = col, alpha = 0.8)
+
+# Previous customizations
+plt.xscale('log') 
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Additional customizations
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
+
+# Add grid() call
+plt.grid(True)
+
+# Show the plot
+plt.show()
