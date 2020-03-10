4f48c0324620bbd2e5b1fa17050eb7bbdd18a245
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-10 10:41:02 +0530

    Create Matplotlib.py

---
+++ Matplotlib.py
@@ -0,0 +1,10 @@
+import matploltlib.pyplot as plt
+#Plot a line with plot()
+plt.plot(xvalue,y_value)
+#Plot a scatter Graph
+plt.scatter(x_value,y_value)
+#Plot a histogram
+#bins: In how many category data to be divided, by default its 10
+plt.hist(value_to_plot, bins=11)
+#Display any graph plotted above
+plt.show()
