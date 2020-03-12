5b62645031e320c0375fa4fd186f4a11f1eeebed
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-12 16:16:56 +0530

    Update DiceGame_UseCase.py

--- Intermediate_Python/DiceGame_UseCase.py
+++ Intermediate_Python/DiceGame_UseCase.py
@@ -31,26 +31,21 @@ print(dice)
 print(step)
 
 #=====================================================================
-random_walk=[]
-random_walk.append(0)
-# Complete the ___
-for x in range(100) :
-    # Set step: last element in random_walk
-    step=random_walk[-1]
+# Initialize random_walk
+random_walk = [0]
 
-    # Roll the dice
+for x in range(100) :
+    step = random_walk[-1]
     dice = np.random.randint(1,7)
 
-    # Determine next step
-    if dice>0 and dice <= 2:
-        step = step - 1
-    elif dice>2 and dice <= 5:
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        st = max(step,step - 1)
+        if st:
+            step=step-1
+    elif dice <= 5:
         step = step + 1
     else:
         step = step + np.random.randint(1,7)
 
-    # append next_step to random_walk
     random_walk.append(step)
-
-# Print random_walk
-print(random_walk)
