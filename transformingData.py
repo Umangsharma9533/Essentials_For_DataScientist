ea739bfa40587b5d36e16951e26f78834100d66b
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-16 11:46:04 +0530

    Update transformingData.py

--- transformingData.py
+++ transformingData.py
@@ -9,3 +9,17 @@ print(homelessness.columns)
 
 # Print the row index of homelessness
 print(homelessness.index)
+
+#=======================================================================
+# Sort homelessness by individual
+
+homelessness_ind = homelessness.sort_values('individuals',ascending=True)
+
+# Print the top few rows
+print(homelessness_ind)
+
+#=======================================================================
+# Sort homelessness by region, then descending family members
+homelessness_reg_fam = homelessness.sort_values(by=['region','family_members'],ascending=[True,False])
+# Print the top few rows
+print(homelessness_reg_fam.head())
