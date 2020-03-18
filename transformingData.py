4cc9cecd0656dc271d27cd666a82bf8405a0b211
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-18 19:38:00 +0530

    Update transformingData.py

--- transformingData.py
+++ transformingData.py
@@ -72,3 +72,12 @@ mojave_homelessness = homelessness[homelessness['state'].isin(canu)]
 
 # See the result
 print(mojave_homelessness)
+#==============================================================================
+# Add total col as sum of individuals and family_members
+homelessness['total']=homelessness['individuals']+homelessness['family_members']
+
+# Add p_individuals col as proportion of individuals
+homelessness['p_individuals']=homelessness['individuals']/homelessness['total']
+
+# See the result
+print(homelessness)
