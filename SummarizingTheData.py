f9246b14bc8c346aea8a7bb98540d780df313aae
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-19 11:31:41 +0530

    Update SummarizingTheData.py

--- SummarizingTheData.py
+++ SummarizingTheData.py
@@ -11,3 +11,19 @@ print(sales['weekly_sales'].mean())
 
 # Print the median of weekly_sales
 print(sales['weekly_sales'].median())
+
+#===========================================================
+# Print the maximum of the date column
+print(sales['date'].max())
+
+# Print the minimum of the date column
+print(sales['date'].min())
+
+#==============================================================
+#Custom use of function in aggregate
+# A custom IQR function
+def iqr(column):
+    return column.quantile(0.75) - column.quantile(0.25)
+
+# Update to print IQR of temperature_c, fuel_price_usd_per_l, & unemployment
+print(sales["temperature_c"].agg(iqr))
