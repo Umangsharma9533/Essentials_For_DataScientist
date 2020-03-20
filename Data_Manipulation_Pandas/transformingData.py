eb476e78fc9f0704e808019674f77edf146352dd
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-20 16:45:45 +0530

    Update transformingData.py

--- Data_Manipulation_Pandas/transformingData.py
+++ Data_Manipulation_Pandas/transformingData.py
@@ -98,3 +98,27 @@ result = high_homelessness_srt[['state','indiv_per_10k']]
 
 # See the result
 print(result)
+
+#====================================================================================
+#Aggregating the values
+# Import NumPy with the alias np
+import numpy as np
+
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+fuc_list=[np.min,np.max,np.mean,np.median]
+
+sales_stats = sales.groupby('type')['weekly_sales'].agg([np.min,np.max,np.mean,np.median])
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby('type')['unemployment','fuel_price_usd_per_l'].agg(fuc_list)
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
+
+
+
+
+#==========================================================================================
+#Aggregatig using pivot table
