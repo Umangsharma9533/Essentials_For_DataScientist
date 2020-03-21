06799c61b32351a46aee815836c864c443fac88d
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-03-21 20:14:10 +0530

    Update Slicing_And_Indexing.py

--- Data_Manipulation_Pandas/Slicing_And_Indexing.py
+++ Data_Manipulation_Pandas/Slicing_And_Indexing.py
@@ -29,10 +29,44 @@ print(temperatures_ind.loc[cities])
 #==============================================================================
 #Multi Level Inedexing
 # Index temperatures by country & city
-temperatures_ind = temperatures.set_index(["country","city"])
+temperatures_ind = temperatures.set_index(["country", "city"])
 
 # List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
-rows_to_keep = temperatures_ind.loc[[('Brazil','Rio De Janeiro'),('Pakistan','Lahore')]]
+rows_to_keep = [("Brazil", "Rio De Janeiro"), ("Pakistan", "Lahore")]
 
 # Subset for rows to keep
-print(rows_to_keep)
+print(temperatures_ind.loc[rows_to_keep])
+#================================================================================
+#Sorted! Sorting index values is similar to sorting values in columns, exectp that you call .sort_index() instead of .sort_values().
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level="city",ascending=True))
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level=["country","city"],ascending=[True,False]))
+#==================================================================================
+# Sort the index of temperatures_ind
+temperatures_srt = temperatures_ind.sort_index()
+
+# Incorrectly subset rows from Pakistan to Russia
+print(temperatures_srt.loc["Pakistan":"Russia"])
+
+# Subset rows from Lahore to Moscow
+print(temperatures_srt.loc["Lahore":"Moscow"])
+
+# Subset rows from Pakistan, Lahore to Russia, Moscow
+print(temperatures_srt.loc[("Pakistan","Lahore"):("Russia","Moscow")])
+#======================================================================================
+#You've seen slicing DataFrames by rows and by columns, but since DataFrames are two dimensional objects it is
+#often natural to slice both dimensions at once. That is, by passing two arguments to .loc[], you can subset by 
+#rows and columns in one go.
+#pandas is loaded as pd. temperatures_srt is indexed by country and city, has a sorted index, and is available.
+# Subset rows from India, Hyderabad to Iraq, Baghdad
+print(temperatures_srt.loc[("India", "Hyderabad"): ("Iraq", "Baghdad")])
+
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[:,"date":"avg_temp_c"])
+
+# Subset in both directions at once
+print(temperatures_srt.loc[("India", "Hyderabad"): ("Iraq", "Baghdad"),"date":"avg_temp_c"])
