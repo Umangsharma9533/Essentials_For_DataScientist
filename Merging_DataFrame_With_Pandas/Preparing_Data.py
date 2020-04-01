77ba0c9ea8b2ce3e7c5b6b76ed1a7a15fb4e872b
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-02 02:18:03 +0530

    Update Preparing_Data.py

--- Merging_DataFrame_With_Pandas/Preparing_Data.py
+++ Merging_DataFrame_With_Pandas/Preparing_Data.py
@@ -63,3 +63,89 @@ medals['Bronze'] = bronze['Total']
 
 # Print the head of medals
 print(medals.head())
+
+
+'''
+Sorting DataFrame with the Index & columns
+It is often useful to rearrange the sequence of the rows of a DataFrame by sorting.
+You don't have to implement these yourself; the principal methods for doing this are .sort_index() and .sort_values().
+
+In this exercise, you'll use these methods with a DataFrame of temperature values indexed by month names.
+You'll sort the rows alphabetically using the Index and numerically using a column.
+Notice, for this data, the original ordering is probably most useful and intuitive:
+the purpose here is for you to understand what the sorting methods do.
+'''
+# Import pandas
+import pandas as pd
+
+# Read 'monthly_max_temp.csv' into a DataFrame: weather1
+weather1 = pd.read_csv('monthly_max_temp.csv',index_col='Month')
+
+# Print the head of weather1
+print(weather1.head())
+
+# Sort the index of weather1 in alphabetical order: weather2
+weather2 = weather1.sort_index(ascending=True)
+
+# Print the head of weather2
+print(weather2.head())
+
+# Sort the index of weather1 in reverse alphabetical order: weather3
+weather3 = weather1.sort_index(ascending=False)
+
+# Print the head of weather3
+print(weather3.head())
+
+# Sort weather1 numerically using the values of 'Max TemperatureF': weather4
+weather4 = weather1.sort_values(by='Max TemperatureF',ascending=True)
+
+# Print the head of weather4
+print(weather4.head())
+
+'''
+Reindexing DataFrame from a list
+Sorting methods are not the only way to change DataFrame Indexes. There is also the .reindex() method.
+
+In this exercise, you'll reindex a DataFrame of quarterly-sampled mean temperature values to contain monthly 
+samples (this is an example of upsampling or increasing the rate of samples, which you may recall from the pandas Foundations course).
+
+The original data has the first month's abbreviation of the quarter (three-month interval) on the Index, namely 
+Apr, Jan, Jul, and Oct. This data has been loaded into a DataFrame called weather1 and has been printed in its entirety
+in the IPython Shell. Notice it has only four rows (corresponding to the first month of each quarter) and that the rows 
+are not sorted chronologically.
+
+You'll initially use a list of all twelve month abbreviations and subsequently apply the .ffill() method to forward-fill 
+the null entries when upsampling. This list of month abbreviations has been pre-loaded as year.
+'''
+# Import pandas
+import pandas as pd
+
+# Reindex weather1 using the list year: weather2
+weather2 = weather1.reindex(year)
+
+# Print weather2
+print(weather2)
+
+# Reindex weather1 using the list year with forward-fill: weather3
+weather3 = weather1.reindex(year).ffill()
+
+# Print weather3
+print(weather3)
+
+'''
+Reindexing using another DataFrame Index
+'''
+# Import pandas
+import pandas as pd
+
+# Reindex names_1981 with index of names_1881: common_names
+common_names = names_1981.reindex(names_1881.index)
+
+# Print shape of common_names
+print(common_names.shape)
+
+# Drop rows with null counts: common_names
+common_names = common_names.dropna()
+
+# Print shape of new common_names
+print(common_names.shape)
