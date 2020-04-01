f8b9a91bf17f1e8316af0c08eb5a123fca0406d5
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-02 00:54:11 +0530

    Update Preparing_Data.py

--- Merging_DataFrame_With_Pandas/Preparing_Data.py
+++ Merging_DataFrame_With_Pandas/Preparing_Data.py
@@ -12,3 +12,54 @@ gold = pd.read_csv('Gold.csv')
 
 # Print the first five rows of gold
 print(gold.head())
+
+'''
+Reading DataFrames from multiple files in a loop
+As you saw in the video, loading data from multiple files into DataFrames is more efficient in a loop or a list comprehension.
+
+Notice that this approach is not restricted to working with CSV files. That is, even if your data comes in other formats, as long as pandas has a suitable data import function, you can apply a loop or comprehension to generate a list of DataFrames imported from the source files.
+
+Here, you'll continue working with The Guardian's Olympic medal dataset.
+'''
+# Import pandas
+import pandas as pd
+
+# Create the list of file names: filenames
+filenames = ['Gold.csv', 'Silver.csv', 'Bronze.csv']
+
+# Create the list of three DataFrames: dataframes
+dataframes = []
+for filename in filenames:
+    dataframes.append(pd.read_csv(filename))
+
+# Print top 5 rows of 1st DataFrame in dataframes
+print(dataframes[0].head())
+
+
+'''
+Combining DataFrames from multiple data files
+In this exercise, you'll combine the three DataFrames from earlier exercises - 
+gold, silver, & bronze - into a single DataFrame called medals. The approach you'll use here is clumsy. 
+Later on in the course, you'll see various powerful methods that are frequently used in practice for concatenating or merging DataFrames.
+
+Remember, the column labels of each DataFrame are NOC, Country, and Total, where NOC is a three-letter code
+for the name of the country and Total is the number of medals of that type won.
+'''
+# Import pandas
+import pandas as pd
+
+# Make a copy of gold: medals
+medals = gold.copy()
+
+# Create list of new column labels: new_labels
+new_labels = ['NOC', 'Country', 'Gold']
+
+# Rename the columns of medals using new_labels
+medals.columns = new_labels
+
+# Add columns 'Silver' & 'Bronze' to medals
+medals['Silver'] = silver['Total']
+medals['Bronze'] = bronze['Total']
+
+# Print the head of medals
+print(medals.head())
