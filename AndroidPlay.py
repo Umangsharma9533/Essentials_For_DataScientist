f75f2599fc38a32e9d3952419a9a4804e54fd24f
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-01 10:26:13 +0530

    Update AndroidPlay.py

--- AndroidPlay.py
+++ AndroidPlay.py
@@ -34,12 +34,30 @@ for col in cols_to_clean:
 '''
 3. Exploring app categories
 
-With more than 1 billion active users in 190 countries around the world, Google Play continues to be an important distribution platform to build a global audience. For businesses to get their apps in front of users, it's important to make them more quickly and easily discoverable on Google Play. To improve the overall search experience, Google has introduced the concept of grouping apps into categories.
-
+With more than 1 billion active users in 190 countries around the world, Google Play continues to be an important
+distribution platform to build a global audience. 
+For businesses to get their apps in front of users, it's important to make them more quickly and easily discoverable on Google Play.
+To improve the overall search experience, Google has introduced the concept of grouping apps into categories.
 This brings us to the following questions:
+Which category has the highest share of (active) apps in the market?
+Is any specific category dominating the market Which categories have the fewest number of apps?
+We will see that there are 33 unique app categories present in our dataset. Family and Game apps 
+have the highest market prevalence. Interestingly, Tools, Business and Medical apps are also at the top.
+'''
+import plotly
+plotly.offline.init_notebook_mode(connected=True)
+import plotly.graph_objs as go
 
-    Which category has the highest share of (active) apps in the market?
-    Is any specific category dominating the market?
-    Which categories have the fewest number of apps?
+# Print the total number of unique categories
+num_categories = len(apps['Category'].unique())
+print('Number of categories = ', num_categories)
 
-We will see that there are 33 unique app categories present in our dataset. Family and Game apps have the highest market prevalence. Interestingly, Tools, Business and Medical apps are also at the top.'''
+# Count the number of apps in each 'Category' and sort them in descending order
+num_apps_in_category = apps['Category'].value_counts().sort_values(ascending = False)
+
+data = [go.Bar(
+        x = num_apps_in_category.index, # index = category name
+        y = num_apps_in_category.values, # value = count
+)]
+
+plotly.offline.iplot(data)
