c2e7be49cc89342d9f31e6f6a018667ee7cb05fe
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-02 04:12:57 +0530

    Update Concatenating_data.py

--- Merging_DataFrame_With_Pandas/Concatenating_data.py
+++ Merging_DataFrame_With_Pandas/Concatenating_data.py
@@ -86,3 +86,112 @@ weather = pd.concat(weather_list,axis=1)
 
 # Print weather
 print(weather)
+
+'''
+Reading multiple files to build a DataFrame
+'''
+#Initialize an empyy list: medals
+medals =[]
+
+for medal in medal_types:
+    # Create the file name: file_name
+    file_name = "%s_top5.csv" % medal
+    # Create list of column names: columns
+    columns = ['Country', medal]
+    # Read file_name into a DataFrame: medal_df
+    medal_df = pd.read_csv(file_name,header=0,index_col='Country',names=columns)
+    # Append medal_df to medals
+    medals.append(medal_df)
+
+# Concatenate medals horizontally: medals_df
+medals_df = pd.concat(medals,axis='columns')
+
+# Print medals_df
+print(medals_df)
+
+'''
+Concatenation , Keys and Multi Indexes
+'''
+for medal in medal_types:
+
+    file_name = "%s_top5.csv" % medal
+    
+    # Read file_name into a DataFrame: medal_df
+    medal_df = pd.read_csv(file_name,index_col='Country')
+    
+    # Append medal_df to medals
+    medals.append(medal_df)
+    
+# Concatenate medals: medals
+medals = pd.concat(medals,keys=['bronze', 'silver', 'gold'])
+
+# Print medals in entirety
+print(medals)
+
+'''
+Slicing MultiIndexed DataFrames
+This exercise picks up where the last ended (again using The Guardian's Olympic medal dataset).
+
+You are provided with the MultiIndexed DataFrame as produced at the end of the preceding exercise. Your task is to sort the DataFrame and to use the pd.IndexSlice to extract specific slices. Check out this exercise from Manipulating DataFrames with pandas to refresh your memory on how to deal with MultiIndexed DataFrames.
+
+pandas has been imported for you as pd and the DataFrame medals is already in your namespace.
+'''
+# Sort the entries of medals: medals_sorted
+medals_sorted = medals.sort_index(level=0)
+
+# Print the number of Bronze medals won by Germany
+print(medals_sorted.loc[('bronze','Germany')])
+
+# Print data about silver medals
+print(medals_sorted.loc['silver'])
+
+# Create alias for pd.IndexSlice: idx
+idx = pd.IndexSlice
+
+# Print all the data on medals won by the United Kingdom
+print(medals_sorted.loc[idx[:,'United Kingdom'],:])
+
+'''
+'''
+# Concatenate dataframes: february
+february = pd.concat(dataframes,keys=['Hardware', 'Software', 'Service'],axis=1)
+
+# Print february.info()
+print(february.info())
+
+# Assign pd.IndexSlice: idx
+idx = pd.IndexSlice
+
+# Create the slice: slice_2_8
+slice_2_8 = february.loc['2015-2-2':'2015-2-8', idx[:, 'Company']]
+
+# Print slice_2_8
+print(slice_2_8)
+
+'''
+Concatenating DataFrames from a dict
+You're now going to revisit the sales data you worked with earlier in the chapter.
+Three DataFrames jan, feb, and mar have been pre-loaded for you. 
+Your task is to aggregate the sum of all sales over the 'Company' column into a single DataFrame.
+You'll do this by constructing a dictionary of these DataFrames and then concatenating them.
+'''
+# Make the list of tuples: month_list
+month_list = [('january', jan), ('february', feb),('march', mar)]
+
+# Create an empty dictionary: month_dict
+month_dict = {}
+
+for month_name, month_data in month_list:
+
+    # Group month_data: month_dict[month_name]
+    month_dict[month_name] = month_data.groupby(by='Company').sum()
+
+# Concatenate data in month_dict: sales
+sales = pd.concat(month_dict)
+
+# Print sales
+print(sales)
+
+# Print all sales by Mediacore
+idx = pd.IndexSlice
+print(sales.loc[idx[:, 'Mediacore'], :])
