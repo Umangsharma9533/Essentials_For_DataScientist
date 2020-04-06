e65027cfbab37c1a7378fd69fd40e1b2915b341b
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-06 10:24:22 +0530

    Create CaseStudy.py

---
+++ Merging_DataFrame_With_Pandas/CaseStudy.py
@@ -0,0 +1,34 @@
+#Import pandas
+import pandas as pd
+
+# Create file path: file_path
+file_path = 'Summer Olympic medallists 1896 to 2008 - EDITIONS.tsv'
+
+# Load DataFrame from file_path: editions
+editions = pd.read_csv(file_path,sep='\t')
+
+# Extract the relevant columns: editions
+editions = editions[['Edition', 'Grand Total', 'City','Country']]
+
+# Print editions DataFrame
+print(editions)
+
+'''
+Loading IOC codes DataFrame
+'''
+
+# Import pandas
+import pandas as pd
+
+# Create the file path: file_path
+file_path = 'Summer Olympic medallists 1896 to 2008 - IOC COUNTRY CODES.csv'
+
+# Load DataFrame from file_path: ioc_codes
+ioc_codes = pd.read_csv(file_path,sep=',')
+
+# Extract the relevant columns: ioc_codes
+ioc_codes = ioc_codes[['Country','NOC']]
+
+# Print first and last 5 rows of ioc_codes
+print(ioc_codes.head())
+print(ioc_codes.tail())
