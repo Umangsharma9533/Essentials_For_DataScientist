be64107b8ccc1bdcb9ba5d46d4fb5d0edc2e1093
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-20 01:15:50 +0530

    Update IntroductionToMatplotlib.py

--- IntroductionToMatplotlib.py
+++ IntroductionToMatplotlib.py
@@ -35,3 +35,56 @@ ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"], color="r",ma
 
 # Call show to display the resulting plot
 plt.show()
+#==========================================================
+#Adding the label and title to the representation of above graph
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"])
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"])
+
+# Customize the x-axis label
+ax.set_xlabel("Time (months)")
+
+# Customize the y-axis label
+ax.set_ylabel("Precipitation (inches)")
+
+# Add the title
+ax.set_title("Weather patterns in Austin and Seattle")
+
+# Display the figure
+plt.show()
+#=======================================================
+# Create a Figure and an array of subplots with 2 rows and 2 columns
+fig, ax = plt.subplots(2, 2)
+#===This will create in total 4 subplots and shape of ax will be(2,2) : means 2 rows and 2 columns
+#we can use the different subplot by using the index values as used below
+# If the ax is 1D then we will use same way using index but this time the reference of index will be 1D for eg: ax[1].plot()
+
+# Addressing the top left Axes as index 0, 0, plot month and Seattle precipitation
+ax[0, 0].plot(seattle_weather['MONTH'], seattle_weather['MLY-PRCP-NORMAL'])
+
+# In the top right (index 0,1), plot month and Seattle temperatures
+ax[0, 1].plot(seattle_weather['MONTH'], seattle_weather['MLY-TAVG-NORMAL'])
+
+# In the bottom left (1, 0) plot month and Austin precipitations
+ax[1,0].plot(austin_weather['MONTH'], austin_weather['MLY-PRCP-NORMAL'])
+
+# In the bottom right (1, 1) plot month and Austin temperatures
+ax[1,1].plot(austin_weather['MONTH'], austin_weather['MLY-TAVG-NORMAL'])
+plt.show()
+
+#================================================================
+# Create a figure and an array of axes: 2 rows, 1 column with shared y axis
+fig, ax = plt.subplots(2, 1, sharey=True)
+# sharey : its is used if we want the y axis to be same across all of the subplots
+
+
+# Plot Seattle precipitation data in the top axes
+ax[0].plot(seattle_weather['MONTH'], seattle_weather['MLY-PRCP-NORMAL'], color = 'b')
+ax[0].plot(seattle_weather['MONTH'], seattle_weather['MLY-PRCP-25PCTL'], color = 'b', linestyle = '--')
+ax[0].plot(seattle_weather['MONTH'], seattle_weather['MLY-PRCP-75PCTL'], color = 'b', linestyle = '--')
+
+# Plot Austin precipitation data in the bottom axes
+ax[1].plot(austin_weather['MONTH'], austin_weather['MLY-PRCP-NORMAL'], color = 'r')
+ax[1].plot(austin_weather['MONTH'], austin_weather['MLY-PRCP-25PCTL'], color = 'r', linestyle = '--')
+ax[1].plot(austin_weather['MONTH'], austin_weather['MLY-PRCP-75PCTL'], color = 'r', linestyle = '--')
+
+plt.show()
