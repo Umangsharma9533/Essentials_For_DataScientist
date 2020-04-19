18c9af1e7aba8310b0ed8d7dcf12cfa373c6de2c
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-20 03:13:30 +0530

    Update PlottingTimeSeriesDataMAtplotlib.py

--- PlottingTimeSeriesDataMAtplotlib.py
+++ PlottingTimeSeriesDataMAtplotlib.py
@@ -55,3 +55,32 @@ ax2.plot(climate_change.index, climate_change['relative_temp'], color='blue')
 # Means the color of the y axis will change for ax in the labelling
 ax.tick_params('y', colors=color)
 plt.show()
+
+#====================================================
+fig, ax = plt.subplots()
+
+# Plot the relative temperature data
+ax.plot(climate_change.index,climate_change['relative_temp'])
+
+# Annotate the date at which temperatures exceeded 1 degree
+# used to put desciption in the graph explaining key points 
+ax.annotate('>1 degree',(pd.Timestamp('2015-10-06'), 1))
+
+plt.show()
+
+#======================================================
+fig, ax = plt.subplots()
+
+# Plot the CO2 levels time-series in blue
+plot_timeseries(ax,climate_change.index, climate_change['co2'], 'blue', "Time (years)", "CO2 levels")
+
+# Create an Axes object that shares the x-axis
+ax2 = ax.twinx()
+
+# Plot the relative temperature data in red
+plot_timeseries(ax2,climate_change.index, climate_change['relative_temp'], 'red', "Time (years)", "Relative temp (Celsius)")
+
+# Annotate point with relative temperature >1 degree
+ax2.annotate(">1 degree", xytext=(pd.Timestamp('2008-10-06'),-0.2), xy=(pd.Timestamp('2015-10-06'),1), arrowprops={"arrowstyle":"->","color":"gray"})
+
+plt.show()
