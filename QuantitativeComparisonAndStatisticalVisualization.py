5b5972fba5b09c01ff7f1e52d9622e2c7c322b6d
Umang Sharma <40338157+Umangsharma9533@users.noreply.github.com>
2020-04-20 23:32:27 +0530

    Create QuantitativeComparisonAndStatisticalVisualization.py

---
+++ QuantitativeComparisonAndStatisticalVisualization.py
@@ -0,0 +1,13 @@
+fig, ax = plt.subplots()
+
+# Plot a bar-chart of gold medals as a function of country
+ax.bar(medals.index,medals['Gold'])
+
+# Set the x-axis tick labels to the country names
+#this will roate the labels on x axis by 90 degree
+ax.set_xticklabels(medals.index,rotation=90)
+
+# Set the y-axis label
+ax.set_ylabel("Number of medals")
+
+plt.show()
